a2952ab860416f12fdcf18970ac4462b3a3c523e
TwelveNight <1315640046@qq.com>
2025-05-20 23:53:25 +0800

    feat(backend): 初始化 Spring Boot 3 项目骨架，添加 Web, Security, MyBatisPlus, Redis, MySQL, OpenAPI

---
+++ backend/src/main/java/com/example/campusbuddy/CampusbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.campusbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusbuddyApplication.class, args);
+	}
+
+}
