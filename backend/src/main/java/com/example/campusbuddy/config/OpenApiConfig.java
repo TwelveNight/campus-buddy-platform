4e6273fb26e948f40d017d64ec9f8e305ec2ac24
TwelveNight <1315640046@qq.com>
2025-05-23 23:31:48 +0800

    chore:请求携带token

--- backend/src/main/java/com/example/campusbuddy/config/OpenApiConfig.java
+++ backend/src/main/java/com/example/campusbuddy/config/OpenApiConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,6 +34,7 @@ public class OpenApiConfig {
                                         .scheme("bearer")
                                         .bearerFormat("JWT")
                                         .in(SecurityScheme.In.HEADER)
-                                        .name("Authorization")));
+                                        .name("Authorization")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt")); // 全局生效
     }
 }
