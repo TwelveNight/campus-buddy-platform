c20ef6414efef58150fb06e62b5239fa3161263c
TwelveNight <1315640046@qq.com>
2025-05-24 20:25:54 +0800

    chore:delete useless code

--- backend/src/main/java/com/example/campusbuddy/service/StudyGroupService.java
+++
@@ -1,8 +0,0 @@
-package com.example.campusbuddy.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.campusbuddy.entity.StudyGroup;
-
-public interface StudyGroupService extends IService<StudyGroup> {
-    // 可扩展自定义方法
-}
